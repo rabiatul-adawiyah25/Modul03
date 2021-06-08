b6658df7a62eff03681835b436b497027c0d8b66
rabiatul-adawiyah25 <rabiatuladawiyahhs@gmail.com>
2021-06-08 22:04:23 +0800

    Menampilkan Frame Pertama Kali

--- src/GuiByText.java
+++ src/GuiByText.java
@@ -3,10 +3,14 @@ import javax.swing.*;
 public class GuiByText extends JFrame {
     GuiByText(){
         setSize(640,480);
+        setLocationRelativeTo(null);
+        setTitle("Gui By Text");
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
     }
 
     public static void main(String[] args){
         GuiByText guiByText = new GuiByText();
         guiByText.setVisible(true);
+
     }
 }
